d67c8bb56d9ed0d021f7ec24451ac02731efe869
Mike <mike.mamie@gmx.ch>
2025-12-10 14:14:29 +0100

    lambda integrated (ich push uf main what you gonna do)

--- main.tf
+++ main.tf
@@ -13,6 +13,42 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-basic-demo-bucket-12345"
+resource "aws_lambda_function" "api_handler" {
+  function_name = "m346-api-handler"
+  role = "arn:aws:iam::654654587245:role/LabRole"
+  runtime = "python3.9"
+  handler = "lambda_function.lambda_handler"
+
+  filename = ""
+  source_code_hash = filebase65sha256("")
+}
+resource "aws_apigatewayv2_api" "http_api" {
+  name = "m346-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri = aws_lambda_function.api_handler.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  
+}
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
 }
